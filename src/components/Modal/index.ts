d2811010ed63f911b84454471a547196a7f7ec93
Andrei Bernatski <iandy1731@gmail.com>
2023-07-18 19:36:15 +0300

    Updates

--- src/components/Modal/index.ts
+++
@@ -1,3 +0,0 @@
-import { lazy } from 'react';
-
-export const Modal = lazy(() => import('./modal'));
--- src/utils/hooks/redux-hooks.ts
+++
@@ -1,6 +0,0 @@
-import { RootState, store } from '@store/index';
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-
-export type DispatchType = typeof store.dispatch;
-export const useDispatchTyped: () => DispatchType = useDispatch;
-export const useSelectorTyped: TypedUseSelectorHook<RootState> = useSelector;
